14a5c0e3351af83cd4c6a4867a1b86e11cbe04cb
wraj <waghrajesh6@gmail.com>
2017-04-08 13:59:19 +0530

    Implemented concerns for managing image generations

--- app/models/port_folio.rb
+++ app/models/port_folio.rb
@@ -1,15 +1,13 @@
 class PortFolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image 
 	scope :ror_items, -> { where(subtitle: "Ruby on Rails") }
 	scope :angular, -> { where(subtitle: "Angular") }
 
 	after_initialize :set_defaults
 
-
-	private
-
 	def set_defaults
-		self.main_image ||= "http://placehold.it/600x400"
-		self.thumb_image ||= "http://placehold.it/350x200" 
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 	end
 end
