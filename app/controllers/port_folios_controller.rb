6524f9bee57eed05d05acdad40fba9d55836dcab
wraj <waghrajesh6@gmail.com>
2017-08-27 20:48:43 +0530

    Implemented form configurations for nested attributes

--- app/controllers/port_folios_controller.rb
+++ app/controllers/port_folios_controller.rb
@@ -9,10 +9,11 @@ class PortFoliosController < ApplicationController
 
   def new
   	@portfolio_item = PortFolio.new
+    3.times{ @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = PortFolio.new(params.require(:port_folio).permit(:title, :subtitle, :body))
+    @portfolio_item = PortFolio.new(params.require(:port_folio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to port_folios_path, notice: 'PortFolio was successfully created.' }
