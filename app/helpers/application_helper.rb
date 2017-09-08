6986b1ca5b115010734d2a08f3043c2cc9f8594c
wraj <waghrajesh6@gmail.com>
2017-09-08 23:44:56 +0530

    Refactored alerts into helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,4 +56,16 @@ module ApplicationHelper
 	def is_active? path
 		"active" if current_page? path
 	end
+
+	def alerts
+		alert = (flash[:alert] || flash[:notice] || flash[:notice])	
+		if alert
+			alert_generator alert
+		end		
+	end
+
+	def alert_generator msg
+		js add_gritter(msg, title: "Rajesh's Portfolio", sticky: false, time: 4000)
+	end
+
 end
