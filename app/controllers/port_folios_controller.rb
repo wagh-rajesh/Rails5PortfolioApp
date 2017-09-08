3c0602f11ca944350b1f0b4efe7bf2cc822eff93
wraj <waghrajesh6@gmail.com>
2017-09-08 14:13:17 +0530

    Implemented initial file upload system carrierwave

--- app/controllers/port_folios_controller.rb
+++ app/controllers/port_folios_controller.rb
@@ -68,6 +68,8 @@ class PortFoliosController < ApplicationController
     params.require(:port_folio).permit(:title,
                                         :subtitle,
                                         :body,
+                                        :main_image,
+                                        :thumb_image,
                                         technologies_attributes: [:name]
                                       )
   end  
