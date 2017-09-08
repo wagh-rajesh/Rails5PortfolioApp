5bef53391856faef950fe9d576bf4eca2b0876a0
wraj <waghrajesh6@gmail.com>
2017-09-08 19:51:06 +0530

    Finished carrierwave installations

--- app/models/port_folio.rb
+++ app/models/port_folio.rb
@@ -2,10 +2,13 @@ class PortFolio < ApplicationRecord
 
 	has_many :technologies
 
-	accepts_nested_attributes_for :technologies, 
-								  							reject_if: lambda { |attrs| attrs['name'].blank? }
+	accepts_nested_attributes_for :technologies, reject_if: lambda { |attrs| attrs['name'].blank? }
+
 	include Placeholder
 	validates_presence_of :title, :body, :main_image, :thumb_image 
+
+	mount_uploader :thumb_image, PortfolioUploader
+	mount_uploader :main_image, PortfolioUploader
 
 	scope :ror_items, -> { where(subtitle: "Ruby on Rails") }
 	scope :angular, -> { where(subtitle: "Angular") }
